14714a4df3e85c29a00ccda7a5f507690ff094c5
wawilow <wawilow@protonmail.com>
2025-04-03 05:10:36 -0400

    even more sudo to script

--- devops/linux-swapfile.sh
+++ devops/linux-swapfile.sh
@@ -1,12 +1,12 @@
 echo 'this scipt will create 4gb /swapfile'
 echo 'ps u need sudo access'
-swapon --show
-df -h
+sudo swapon --show
+sudo df -h
 
 sudo dd if=/dev/zero of=/swapfile bs=1G count=4
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
-swapon --show
+sudo swapon --show
 
 echo "/swapfile none swap sw 0 0" | sudo tee -a /etc/fstab
